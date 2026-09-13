50dc02a801f21dc74e5992b53d5ea5f3fdec0cb7
unknown <bulat3703@gmail.com>
2026-09-14 01:06:37 +0300

    New Lab1 tests

--- lab1-js-core/solutions/student-21/solution.js
+++ lab1-js-core/solutions/student-21/solution.js
@@ -25,7 +25,7 @@ function getReviewerNumber(number, lab) {
 
 function getVariant(number, variants) {
     // 2.2 Функция определяющая номер варианта, исходя из количества вариантов
-    return number % variants;
+    return ((number - 1) % variants) + 1;
 }
 
 function calculate(a, b, op) {
@@ -178,6 +178,16 @@ function processArrays() {
     //    - преобразовать в массив имен
     //    - отсортировать по алфавиту
     const activeUserNames = users.filter(user => user.isActive).map(user => user.name).sort((a, b) => a.localeCompare(b));
+
+    return {
+        squares,
+        activeUsers,
+        victoria,
+        sum,
+        sortedByAge,
+        allAdults,
+        activeUserNames
+    };
 }
 
 // ===== ЗАДАНИЕ 5: Менеджер задач =====
@@ -304,7 +314,7 @@ function taskClasses() {
     // 6.2 Класс Car (наследуется от Vehicle)
     // Добавьте новое свойство numDoors (количество дверей).
     class Car extends Vehicle {
-        constructor(make, model, year, numDoors) {
+        constructor(make, model, year, numDoors = 4) {
             super(make, model, year); 
             this.numDoors = numDoors;
         }
@@ -325,7 +335,7 @@ function taskClasses() {
     // 6.3 Класс ElectricCar (наследуется от Car)
     // Добавьте новое свойство batteryCapacity (емкость батареи в кВт·ч).
     class ElectricCar extends Car {
-        constructor(make, model, year, numDoors, batteryCapacity) {
+        constructor(make, model, year, numDoors = 4, batteryCapacity) {
             super(make, model, year, numDoors);
             this.batteryCapacity = batteryCapacity;
         }
@@ -346,8 +356,8 @@ function taskClasses() {
     // ===== ЗАДАНИЕ 7: Каррирование =====
     // Создайте функцию createVehicleFactory, которая возвращает функцию
     // для создания транспортных средств определенного типа (каррирование).
-    const createVehicleFactory = (vehicleType) => (make, model, year) => {
-        return new vehicleType(make, model, year);
+    const createVehicleFactory = (vehicleType) => (...args) => {
+        return new vehicleType(...args);
     };
 
     return { Vehicle, Car, ElectricCar, createVehicleFactory };
@@ -428,31 +438,167 @@ function validateDate(date) {
 function runTests() {
     console.log("=== ТЕСТИРОВАНИЕ ===");
 
-    // Тест 1: getReviewerNumber
-    console.assert(getReviewerNumber(5, 1) === 6, "Тест получения ревьюера провален");
+    console.log("\n== ЗАДАНИЕ 1 ==")
 
-    // Тест 2: calculate
+    // Тест 1: simpleTask
+    simpleTask();
+
+    console.log("== Тесты задания 1 завершены ==")
+
+    console.log("\n== ЗАДАНИЕ 2 ==")
+
+    // Тест 2: getReviewerNumber
+    console.assert(getReviewerNumber(20, 1) === 21, "Тест получения ревьюера провален");
+    console.assert(getReviewerNumber(29, 1) === 0, "Тест получения ревьюера провален");
+
+    // Тест 3: getVariant
+    console.assert(getVariant(21, 4) === 1)
+    console.assert(getVariant(10, 5) === 5)
+    
+    // Тест 4: calculate
     console.assert(calculate(10, 5, '+') === 15, "Тест калькулятора провален");
+    console.assert(calculate(10, 0, '/') === 'На 0 делить нельзя', "Тест калькулятора провален");
+    console.assert(calculate(10, 5, '%') === 'Неизвестная операция', "Тест калькулятора провален");
 
-    // Тест 3: taskManager
-    console.assert((taskManager.getStats() || {}).total === 3, "Тест taskManager провален");
+    // Тест 5: calculateArea
+    console.assert(calculateArea('circle', 5) === Math.PI * 25, "Тест калькулятора площади провален");
+    console.assert(calculateArea('rectangle', 5, 10) === 50, "Тест калькулятора площади провален");
+    console.assert(calculateArea('triangle', 10, 5) === 25, "Тест калькулятора площади провален");
+    console.assert(calculateArea('square', 5) === 'Неизвестная фигура', "Тест калькулятора площади провален");
 
-    // Тест 4: классы и наследование
+    // Тест 6: reverseString:
+    console.assert(reverseString('hello') === 'olleh', "Тест реверса строки провален");
+    console.assert(reverseString('6310') === '0136', "Тест реверса строки провален");
+    console.assert(reverseString('') === '', "Тест реверса строки провален");
+
+    // Тест 7: getRandomNumber
+    const randomNumber = getRandomNumber(1, 10);
+    console.assert(randomNumber >= 1 && randomNumber <= 10, "Тест рандомного числа провален");
+
+    console.log("== Тесты задания 2 завершены ==")
+
+    console.log("\n== ЗАДАНИЕ 3 ==")
+
+    // Тест 8: book getInfo()
+    console.assert(book.getInfo() === '"Старик и море", автор: Эрнест Хемингуэй, год: 1952, страниц: 128', "Тест getInfo провален");
+    
+    // Тест 9: book toggleAvailability
+    console.assert(book.isAvailable === true, "Тест isAvailable провален");
+    console.assert(book.toggleAvailability() === false, "Тест toggleAvailability провален");
+    console.assert(book.toggleAvailability() === true, "Тест toggleAvailability провален");
+
+    // Тест 10: student getAverageGrade
+    console.assert(student.getAverageGrade() === 90, "Тест getAverageGrade провален");
+    
+    // Тест 11: student addGrade
+    student.addGrade('physics', 80)
+    console.assert(student.grades.physics === 80, "Тест addGrade провален");
+
+    console.log("== Тесты задания 3 завершены ==");
+
+    console.log("\n== ЗАДАНИЕ 4 ==");
+
+    const arrayResult = processArrays();
+
+    // Тест 12: forEach
+    console.assert([67, 89, 56, 91].join(',') === '67,89,56,91', "Тест forEach провален");
+
+    // Тест 13: map
+    console.assert(arrayResult.squares.join(',') === 
+    '144,2025,529,4489,1156,7921,3136,8281,729,196', "Тест map провален");
+
+    // Тест 14: filter
+    console.assert(arrayResult.activeUsers.length === 3, "Тест filter провален");
+    console.assert(arrayResult.activeUsers.every(user => user.isActive), 
+    "Тест filter провален");
+
+    // Тест 15: find
+    console.assert(arrayResult.victoria.name === 'Виктория', "Тест find провален");
+    console.assert(arrayResult.victoria.age === 22, "Тест find провален");
+
+    // Тест 16: reduce
+    console.assert(arrayResult.sum === 458, "Тест reduce провален");
+    
+    // Тест 17: sort
+    console.assert(arrayResult.sortedByAge.map(user => user.age).join(',') === 
+    '35,30,28,25,22', "Тест sort провален");
+
+    // Тест 18: every
+    console.assert(arrayResult.allAdults === true, "Тест every провален");
+    
+    // Тест 19: filter map sort
+    console.assert(arrayResult.activeUserNames.join(',') === 
+    'Анна,Виктория,Григорий', "Тест filter map sort провален");
+
+    console.log("== Тесты задания 4 завершены ==");
+    
+    console.log("\n== ЗАДАНИЕ 5 ==");
+
+    // Тест 20: addTask
+    const oldLength = taskManager.tasks.length;
+    taskManager.addTask('Повторить тесты', 'low');
+    console.assert(taskManager.tasks.length === oldLength + 1,
+        "Тест addTask провален");
+
+    const addedTask = taskManager.tasks[taskManager.tasks.length - 1];
+    console.assert(addedTask.completed === false, "Тест addTask провален");
+        
+    // Тест 21: completeTask
+    taskManager.completeTask(addedTask.id);
+    console.assert(addedTask.completed === true, "Тест completeTask провален");
+
+    // Тест 22: deleteTask
+    const lengthBeforeDelete = taskManager.tasks.length;
+    taskManager.deleteTask(addedTask.id);
+
+    console.assert(taskManager.tasks.length === lengthBeforeDelete - 1, "Тест deleteTask провален");
+    console.assert(taskManager.tasks.find(task => task.id === addedTask.id) === undefined, "Тест deleteTask провален");
+    
+    // Тест 23: getTasksByStatus
+    const completedTasks = taskManager.getTasksByStatus(true);
+    const pendingTasks = taskManager.getTasksByStatus(false);
+
+    console.assert(completedTasks.every(task => task.completed === true),
+    "Тест getTasksByStatus провален");
+    console.assert(pendingTasks.every(task => task.completed === false),
+    "Тест getTasksByStatus провален");
+
+    // Тест 24: getStats
+    const stats = taskManager.getStats();
+    
+    console.assert(stats.total === 3, "Тест getStats провален");
+    console.assert(stats.completed === 1, "Тест getStats провален");
+    console.assert(stats.pending === 2, "Тест getStats провален");
+    console.assert(stats.completionRate === 33,
+    "Тест getStats провален");
+
+    console.log("== Тесты задания 5 завершены ==");
+
+    console.log("\n== ЗАДАНИЕ 6 ==");
+
+    // Тест 25: Vehicle
     const { Vehicle, Car, ElectricCar, createVehicleFactory } = taskClasses();
     const vehicle = new Vehicle('Toyota', 'Camry', 2015);
     vehicle.displayInfo();
     console.log(`Возраст: ${vehicle.age} лет`);
-
+    
+    // Тест 26: Car
     const car = new Car('Honda', 'Civic', 2018, 4);
     car.displayInfo();
     car.honk();
-
+    
+    // Тест 27: electricCar
     const electricCar = new ElectricCar('Tesla', 'Model 3', 2020, 4, 75);
     electricCar.displayInfo();
     console.log(`Запас хода: ${electricCar.calculateRange()} км`);
-
+    
+    // Тест 28: Age
     const testVehicle = new Vehicle('Test', 'Model', 2010);
     console.assert(testVehicle.age === (new Date().getFullYear() - 2010), 'Тест возраста провален');
+
+    console.log("== Тесты задания 6 завершены ==");
+
+    console.log("\n== ЗАДАНИЕ 7 ==");
 
     const createCarFactory = createVehicleFactory(Car);
     const myNewCar = createCarFactory('BMW', 'X5', 2022);
@@ -460,8 +606,11 @@ function runTests() {
     myNewCar.displayInfo();
 
     console.log('Всего создано транспортных средств:', Vehicle.getTotalVehicles()); 
+    
+    console.log("== Тесты задания 7 завершены ==");
+    
+    console.log("\n== ЗАДАНИЕ 8 ==");
 
-    //Тест 5: 1 вариант регулярные выражения
     const tests = [
         // 1. Корректный email
         { email: "user@example.com", expected: true },
@@ -510,7 +659,7 @@ function runTests() {
         const result = validateEmail(test.email);
 
         console.log(
-            `Тест ${index + 1}:`,
+            `Тест почты ${index + 1}:`,
             result === test.expected ? "ПРОЙДЕН" : "НЕ ПРОЙДЕН",
             `| Email: "${test.email}"`,
             `| Ожидалось: ${test.expected}`,
